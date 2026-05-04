bb8183ccfec989575f70d5150815754a84982460
yash-pareek-21 <yp883677@gmail.com>
2026-05-04 11:42:34 +0530

    Add files via upload

--- first.cpp
+++ first.cpp
@@ -1,23 +1,18 @@
-// write a c++ program that takes an integer as a input from the user 
-// print all the number between 1 to n that is diviible by 2.
-
-
 #include <iostream>
 using namespace std;
+int main()
+{
+    int r,c;
+    cin >> r >> c;
+    for (int i = 1; i<= r; i++)
+{
+    for(int j = 1; j <=c; j++)
+    {
+        cout<<"*";
 
-int main() {
-    int n;
-    
-    cout << "Enter an integer: ";
-    cin >> n;
-
-    cout << "Numbers divisible by 2 between 1 and " << n << " are:\n";
-
-    for(int i = 1; i <= n; i++) {
-        if(i % 2 == 0) {
-            cout << i << " ";
-        }
     }
 
-    return 0;
+    cout<< endl;
+}
+   return 0;
 }
